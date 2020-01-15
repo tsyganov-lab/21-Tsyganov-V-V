b1c576645ece1900d8227286c8c44c9d5fc9acee
tsyganov-lab <tsyganov.vlad20@yandex.ru>
2020-01-15 15:56:15 +0300

    1

--- WindowsFormsParusnik/Parusnik.cs
+++ WindowsFormsParusnik/Parusnik.cs
@@ -6,13 +6,7 @@ using System.Drawing;
 
 namespace WindowsFormsParusnik
 {
-    public enum Direction
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
+    
 
     public class Parusnik
     {
